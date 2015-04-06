eb687827b4686b537c15fe42a7da22d47385ccfd
fdesimini <frankdesimini@gmail.com>
2015-04-06 15:50:14 -0400

    Added feature model

---
+++ BitmakerLabsDay1/Feature.h
@@ -0,0 +1,13 @@
+//
+//  Feature.h
+//  BitmakerLabsDay1
+//
+//  Created by Frank Desimini on 2015-04-06.
+//  Copyright (c) 2015 Frank Desimini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Feature : NSObject
+
+@end
---
+++ BitmakerLabsDay1/Feature.m
@@ -0,0 +1,13 @@
+//
+//  Feature.m
+//  BitmakerLabsDay1
+//
+//  Created by Frank Desimini on 2015-04-06.
+//  Copyright (c) 2015 Frank Desimini. All rights reserved.
+//
+
+#import "Feature.h"
+
+@implementation Feature
+
+@end
