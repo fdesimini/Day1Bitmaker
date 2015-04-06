cf3a214c019cc043c234be6e5c7d5577fa8818b6
Frank Desimini <frankdesimini@Franks-MacBook-Air.local>
2015-04-06 15:06:15 -0400

    Initial Commit

---
+++ BitmakerLabsDay1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BitmakerLabsDay1
+//
+//  Created by Frank Desimini on 2015-04-06.
+//  Copyright (c) 2015 Frank Desimini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BitmakerLabsDay1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BitmakerLabsDay1
+//
+//  Created by Frank Desimini on 2015-04-06.
+//  Copyright (c) 2015 Frank Desimini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
